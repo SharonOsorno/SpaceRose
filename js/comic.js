e42775dcad813c1ea8362572038abe3e4fabb523
SharonOsorno <slosorno2@misena.edu.co>
2021-11-22 15:26:03 -0500

    comic 001

--- js/comic.js
+++ js/comic.js
@@ -6,14 +6,26 @@ const book = document.querySelector('#book');
 const paper1 = document.querySelector('#p1')
 const paper2 = document.querySelector('#p2')
 const paper3 = document.querySelector('#p3')
+const paper4 = document.querySelector('#p4')
+const paper5 = document.querySelector('#p5')
+const paper6 = document.querySelector('#p6')
+const paper7 = document.querySelector('#p7')
+const paper8 = document.querySelector('#p8')
+
+
+
 
 // Event listeners
 prevBtn.addEventListener("click", goPrevious);
 nextBtn.addEventListener("click", goNext);
 
+
+
+
+
 // Business Logic
 let currentState = 1;
-let numOfPapers = 3;
+let numOfPapers = 8;
 let maxState = numOfPapers + 1;
 
 
@@ -24,7 +36,7 @@ function openBook() {
 }
 
 function closeBook(isFirstPage) {
-    if(isFirstPage) {
+    if (isFirstPage) {
         book.style.transform = "translateX(0%)";
     } else {
         book.style.transform = "translateX(100%)";
@@ -34,8 +46,8 @@ function closeBook(isFirstPage) {
 }
 
 function goNext() {
-    if(currentState < maxState) { 
-        switch(currentState) {
+    if (currentState < maxState) {
+        switch (currentState) {
             case 1:
                 openBook();
                 paper1.classList.add("flipped");
@@ -46,12 +58,32 @@ function goNext() {
                 paper2.style.zIndex = 2;
                 break;
             case 3:
-                closeBook(false);
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
                 break;
-            default: 
-                throw new Error("unkown state");    
+            case 4:
+                paper4.classList.add("flipped");
+                paper4.style.zIndex = 4;
+                break;
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
+                break;
+            case 6:
+                paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
+                break;
+            case 7:
+                paper7.classList.add("flipped");
+                paper7.style.zIndex = 7;
+                break;           
+            case 8:
+                closeBook(false);
+                paper8.classList.add("flipped");
+                paper8.style.zIndex = 8;
+                break;
+            default:
+                throw new Error("unkown state");
         }
 
         currentState++;
@@ -59,21 +91,44 @@ function goNext() {
 }
 
 function goPrevious() {
-    if(currentState > 1) {
-        switch(currentState) {
+    if (currentState > 1) {
+        switch (currentState) {
             case 2:
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 3;
+                paper1.style.zIndex = 8;
                 break;
             case 3:
+
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 2;
+                paper2.style.zIndex = 7;
                 break;
-            case 4: 
-                openBook()
+            case 4:
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
+                paper3.style.zIndex = 6;
+                break;
+
+            case 5:
+                paper4.classList.remove("flipped");
+                paper4.style.zIndex = 5;
+                break;
+
+            case 6:
+                paper5.classList.remove("flipped");
+                paper5.style.zIndex = 4;
+                break;
+            case 7:
+                paper6.classList.remove("flipped");
+                paper6.style.zIndex = 3;
+                break;
+            case 8:
+                paper7.classList.remove("flipped");
+                paper7.style.zIndex = 2;
+                break;
+            case 9:
+                openBook()
+                paper8.classList.add("flipped");
+                paper8.style.zIndex = 1;
                 break;
         }
 
