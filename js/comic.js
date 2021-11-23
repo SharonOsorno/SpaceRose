9e0bdc3e7d5470acbf0d7773ac7a1ce4f8008cef
SharonOsorno <slosorno2@misena.edu.co>
2021-11-23 18:56:15 -0500

    diseño_escenas

--- js/comic.js
+++ js/comic.js
@@ -25,7 +25,7 @@ nextBtn.addEventListener("click", goNext);
 
 // Business Logic
 let currentState = 1;
-let numOfPapers = 8;
+let numOfPapers = 9;
 let maxState = numOfPapers + 1;
 
 
@@ -66,22 +66,11 @@ function goNext() {
                 paper4.style.zIndex = 4;
                 break;
             case 5:
+                closeBook(false);
                 paper5.classList.add("flipped");
                 paper5.style.zIndex = 5;
                 break;
-            case 6:
-                paper6.classList.add("flipped");
-                paper6.style.zIndex = 6;
-                break;
-            case 7:
-                paper7.classList.add("flipped");
-                paper7.style.zIndex = 7;
-                break;           
-            case 8:
-                closeBook(false);
-                paper8.classList.add("flipped");
-                paper8.style.zIndex = 8;
-                break;
+          
             default:
                 throw new Error("unkown state");
         }
@@ -114,22 +103,11 @@ function goPrevious() {
                 break;
 
             case 6:
+                openBook()
                 paper5.classList.remove("flipped");
                 paper5.style.zIndex = 4;
                 break;
-            case 7:
-                paper6.classList.remove("flipped");
-                paper6.style.zIndex = 3;
-                break;
-            case 8:
-                paper7.classList.remove("flipped");
-                paper7.style.zIndex = 2;
-                break;
-            case 9:
-                openBook()
-                paper8.classList.add("flipped");
-                paper8.style.zIndex = 1;
-                break;
+           
         }
 
         currentState--;
